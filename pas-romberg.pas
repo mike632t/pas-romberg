852999687f0f7f013fe42924369774ef15170005
mike632t <1799044+mike632t@users.noreply.github.com>
2026-06-21 02:04:23 +0100

    Updated description

--- pas-romberg.pas
+++ pas-romberg.pas
@@ -4,7 +4,7 @@
 *  Copyright(C) 2026 - MT
 *
 *  Calculates  the  definite integral for a function  fn(x)  using  Romberg
-*  integration. 
+*  method. 
 *
 *  This  program is free software: you can redistribute it and/or modify it
 *  under  the terms of the GNU General Public License as published  by  the
